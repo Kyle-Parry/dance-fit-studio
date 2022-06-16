d8b70280b5384fed57e59f642ac109c3996df9c3
Kyle-Parry <kyleparry01@gmail.com>
2022-06-16 12:54:19 +1000

    changes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -10,6 +10,21 @@ router.post("/login", passport.authenticate("user"), (req, res) => {
 
 // The admin login route will only run with
 // whitelisted IP addresses.
+ipWhiteList = (req, res, next) => {
+  let validIps = ["1.128.108.225", "1.128.105.177"]; // Put your IP whitelist in this array
+
+  if (validIps.includes(req.connection.remoteAddress)) {
+    // IP is ok, so go on
+    console.log("IP ok");
+    next();
+  } else {
+    // Invalid ip
+    console.log("Bad IP: " + req.connection.remoteAddress);
+    const err = new Error("Bad IP: " + req.connection.remoteAddress);
+    next(err);
+  }
+};
+
 const ips = [
   "::ffff:1.128.108.225",
   "::ffff:1.128.105.177",
@@ -18,7 +33,7 @@ const ips = [
 
 router.post(
   "/admin",
-  ipfilter(ips, { mode: "allow" }),
+  ipWhiteList,
   passport.authenticate("admin"),
   (req, res) => {
     res.status(200).send({ msg: "Logged in" });
