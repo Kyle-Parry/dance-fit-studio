0eac4d1338afd4cbde217738b556a2261e2726b7
Kyle-Parry <kyleparry01@gmail.com>
2022-06-16 12:44:18 +1000

    changes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -10,7 +10,11 @@ router.post("/login", passport.authenticate("user"), (req, res) => {
 
 // The admin login route will only run with
 // whitelisted IP addresses.
-const ips = ["1.128.108.225", "1.128.105.177", "::ffff:10.1.90.210"];
+const ips = [
+  "::ffff:1.128.108.225",
+  "::ffff:1.128.105.177",
+  "::ffff:10.1.90.210",
+];
 
 router.post(
   "/admin",
